96a9e1b311fd65421274aa05e6eaf79a9a020f3d
Punvarat2534 <72251854+Punvarat2534@users.noreply.github.com>
2025-02-01 23:57:37 +0700

    'production'

--- cores/db/index.js
+++ cores/db/index.js
@@ -1,6 +1,6 @@
 ﻿//const knex = require("knex")(process.env.DATABASE_URL);
-//const knex = require("knex")('production');
-const knex = require("knex");
+const knex = require("knex")('production');
+//const knex = require("knex");
 const knexFile = require("../config/pg");
 const environment = process.env.NODE_ENV || "development";
 module.exports = knex(knexFile[environment]);
