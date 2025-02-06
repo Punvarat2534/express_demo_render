a5193b070fc6a46619a417b2c3ffef38a3c5e002
Punvarat2534 <72251854+Punvarat2534@users.noreply.github.com>
2025-02-06 10:25:27 +0700

    '06022025'

--- cores/config/db.js
+++ cores/config/db.js
@@ -13,3 +13,38 @@ module.exports = {
     },
   }
 };
+
+
+/*module.exports = {
+  development: {
+    client: "pg",
+    connection: process.env.DATABASE_URI,
+    migrations: {
+      directory: "./db/migrations",
+    }
+  }
+};*/
+
+/*const dbs = knex({
+  client: 'pg',
+  connection: {
+    host: "k2mpg.ddns.net",
+    port: "5432",
+    user: "sa",
+    database:"dbnap",
+    password:"1/sasql64",
+  },
+});*/
+
+/*const pg = require('knex')({
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    //ssl: config['DB_SSL'] ? { rejectUnauthorized: false } : false,
+  },
+});*/
--- cores/db/index.js
+++ cores/db/index.js
@@ -1,4 +1,4 @@
-﻿const knex = require("knex");
+﻿const knex = require("knex")({ client: 'pg' });
 const knexFile = require("../config/db");
 const environment = process.env.NODE_ENV || "development";
 module.exports = knex(knexFile[environment]);
